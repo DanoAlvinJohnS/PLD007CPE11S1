579167c92864385de90ef3a4841f59d3bcc0627e
AlvinJohnDano <qajsdano@tip.edu.ph>
2024-09-09 12:38:15 +0800

    Algorithm to get the Sum, difference, product, quotient, and remainder

---
+++ Algorithm to get the Sum, difference, product, quotient, and remainder.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "\nAlgorithm to get the Sum, difference, product, quotient, and remainder \n"<<endl;
+  //Local Variables
+  int x = 5, y = 2, sum = 0, product = 0, difference = 0;
+  float quotient = 0, remainder = 0;
+  
+  //Formula 
+  sum = x + y;
+  product = x * y;
+  difference = x - y;
+  remainder = x % y;
+  quotient = x /(double)y;
+  
+  //Printing Results
+  cout << "Sum = " << sum << endl;
+  cout << "Product = " << product << endl;
+  cout << "Difference = " << difference << endl;
+  cout << "Quotient = " << quotient << endl;
+  cout << "Remainder = " << remainder << endl;
+
+  return 0;
+}
+
