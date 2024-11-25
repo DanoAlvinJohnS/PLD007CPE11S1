d89b1fa50b412c32f5b5dd07a0179ab5d9403b1b
AlvinJohnDano <qajsdano@tip.edu.ph>
2024-11-25 12:53:11 +0800

    Add files via upload

---
+++ Finals/Exercise 9.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath> // Include cmath for sqrt
+using namespace std;
+
+int main() {
+    double num = 25.0;
+    cout << "Square root of " << num << " is: " << sqrt(num) << endl;
+    return 0;
+}
+
